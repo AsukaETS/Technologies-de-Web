beb2106505e3964a3d80c724a2f5d45bc36ffc19
AsukaETS <asuka.ets@gmail.com>
2016-09-22 09:30:08 +0200

    TD3 Update

--- TD3 - TP3/index.php
+++ TD3 - TP3/index.php
@@ -1,4 +1,4 @@
-<?php include 'connexion.php'; ?>
+<?php include "connexion.php"; ?>
 
 <!DOCTYPE html>
 <html>
@@ -7,8 +7,8 @@
     <title>TD3</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
   </head>
-    <body>
-  <h1>Mes Contacts</h1>
+  <body>
+      <h1>Mes Contacts</h1>
       <table border="1">
           <tr>
               <th>id</th>
@@ -21,14 +21,10 @@
               <th>mail</th>
               <th>annee naissance</th>
           </tr>
-        
       <?php
         $sql="SELECT * FROM contacts";
         $res=$connexion->query($sql);
         $tab=$res->fetchAll(PDO::FETCH_ASSOC);
-        //echo "<pre>";
-        //print_r($res);
-        //echo "</pre>";
 
         foreach($tab as $value) {
             echo "<tr>";
@@ -37,10 +33,10 @@
             }
             echo "</tr>";
         }
-            
-          ?> </table>
+        ?>
+      </table>
         <h1>Requête avec critère</h1>
-          <table border="1">
+        <table border=1>
           <tr>
               <th>id</th>
               <th>nom</th>
@@ -52,52 +48,36 @@
               <th>mail</th>
               <th>annee naissance</th>
           </tr>
-          <?php
-        $sql="SELECT * FROM contacts WHERE ville = 'La Rochelle'";
+        <?php
+        $sql="SELECT * FROM contacts WHERE ville='La Rochelle'";
         $res=$connexion->query($sql);
-        $tab1=$res->fetchAll(PDO::FETCH_ASSOC);
-        //echo "<pre>";
-        //print_r($res);
-        //echo "</pre>";
+        $tab=$res->fetchAll(PDO::FETCH_ASSOC);
 
-        foreach($tab1 as $value) {
+        foreach($tab as $value) {
             echo "<tr>";
             foreach($value as $v) {
                 echo "<td> $v </td>";
             }
             echo "</tr>";
         }
-
-              ?> </table>
-              
-              
-              
-              <h1>Requête d'insertion</h1>
-              
+        ?>
         <?php
-//$sql ="INSERT INTO contacts VALUES (5, 'Jean', 'Michel', 'Rue des patates', 17300, 'Rochefort', 0648596452, 'JeanMichel@gmail.com', 1900)" ;
-//$res=$connexion->exec($sql);
-
-?> 
-        
-        <h1>Requête préparée</h1>
-
-        <?php
- $requete_prepare_1=$connexion->prepare("SELECT * FROM contacts WHERE ville = :ville") ;
-$requete_prepare_1->execute (array('ville'=>'Bordeaux')) ;
-$res=$requete_prepare_1->fetch(PDO::FETCH_OBJ) ;
-echo $res -> id. " "; 
-echo $res -> nom ." "; 
-echo $res -> prenom ." ";
-echo $res -> adresse ." ";
-echo $res -> codePostal." ";
-echo $res -> ville." ";
-echo $res -> telephone. " "; 
-echo $res -> mail. " ";
-echo $res -> anneeNaissance." "; 
-?>
-        
-        <h1>Requête avec critère issu d'un formulaire</h1>
+            /*$ins="INSERT INTO contacts(nom,prenom,adresse,codePostal,ville,telephone,mail,anneNaissance)
+            VALUES('Bond','James','7 rue de lespion',54000,'Spycity', 0254152362, 'bond@gmail.com',1986)";
+            $connexion->exec($ins);*/
+            
+        ?>
+      </table>
+      
+      <h1>Requête préparée</h1>
+          <?php
+          $sql=$connexion->prepare("SELECT * FROM contacts WHERE ville=:nomV");
+          $sql->execute(array('nomV'=>'La Rochelle'));
+          $res=$sql->fetchAll(PDO::FETCH_ASSOC);
+          print_r($res);
+          ?>
+      
+      <h1>Requête avec critère issu d'un formulaire</h1>
       <form id="formulaire" method="POST" action="index.php">
           <fieldset>
           <label for="ville" id="idVille">Ville</label>
@@ -107,37 +87,40 @@ echo $res -> anneeNaissance." ";
           </p>
           </fieldset>
       </form>
-        <table border="1">
-          <tr>
-              <th>id</th>
-              <th>nom</th>
-              <th>prenom</th>
-              <th>adresse</th>
-              <th>code Postal</th>
-              <th>Ville</th>
-              <th>telephone</th>
-              <th>mail</th>
-              <th>annee naissance</th>
-          </tr>
       <?php
         if(isset($_POST["send"])) {
             if (isset($_POST["VILLE"])) {
                 $ville=$_POST["VILLE"];
                 $sql=$connexion->prepare("SELECT * FROM contacts WHERE ville=?");
                 $sql->execute(array($ville));
-            $res=$sql->fetchAll(PDO::FETCH_ASSOC);
-                
+                $res=$sql->fetchAll(PDO::FETCH_ASSOC);
+                print_r($res);
             }
         }
-foreach($res as $value) {
-            echo "<tr>";
-            foreach($value as $v) {
-                echo "<td> $v </td>";
+      ?>
+      
+      <h1>Requête d'insertion avec données issues d'un formulaire</h1>
+      <form id="formulaire" method="POST" action="index.php">
+          <fieldset>
+          <label for="nom" id="idNom">Nom</label>
+          <input type="text" id="nom" name="nom" required />
+          <label for="prenom" id="idP">Prénom</label>
+          <input type="text" id="prenom" name="prenom" required />
+          <label for="adresse" id="idA">Adresse</label>
+          <input type="text" id="adresse" name="adresse" required />
+          <p class="submit">
+          <input type="submit" id="btnSubmit" value="Continuer" name="send" />
+          </p>
+          </fieldset>
+      </form>
+      <?php
+        if(isset($_POST["send"])) {
+            if (isset($_POST["nom"]) && isset($_POST["prenom)"]) && isset($_POST["adresse"])) {
+                $nom=$_POST["nom"];
+                $prenom=$_POST["prenom"];
+                $adresse=$_POST["adresse"];
             }
-            echo "</tr>";
         }
-        
-   
-?>
-</body>      
+        ?>
+  </body>
 </html>
